9f0bdb593d91c11d3054260a13e4d03a697c3a95
Jeavanni Walker <jwalker@wlapcs.org>
2017-03-07 00:50:06 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup(){
+    createCanvas(600,800);
+    background(40, 237, 204);
+}
+
+function draw(){
+    
+    line(30,100,200,54);
+     line(25,80,300,200);
+    
+    stroke(93, 173, 50);
+    fill(132, 23, 80);
+    rect(290,100,250,250);
+    rect(100,60,100);
+    
+    strokeWeight(4);
+    fill(111, 219, 179);
+    ellipse(50,250,80,80);
+    ellipse(60,200,91,79);
+    
+    line(4,0,5,200);
+    
+ 
+}
